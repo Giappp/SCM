58052c4ad23909a95abc5fb56620dd09273e7bc4
Thanh <thanh.lv.2201@aptechlearning.edu.vn>
2023-09-20 20:18:59 +0700

    add module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.example.scm {
     requires javafx.controls;
     requires javafx.fxml;
             
-                        requires org.kordamp.bootstrapfx.core;
-            
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+
     opens com.example.scm to javafx.fxml;
     exports com.example.scm;
 }
